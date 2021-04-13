237a3a051bb8ef75135aaff236213e0f072e1a9f
Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
2021-04-13 15:03:57 +0200

    Revert "Add 'set-port-binary/text-mode!' procedure for MinGW."
    This reverts commit 57de0742475e128bd1bc71957b3e73ff29648ded.

--- module/ice-9/ports.scm
+++ module/ice-9/ports.scm
@@ -69,7 +69,6 @@
             port-for-each
             flush-all-ports
             %make-void-port
-            set-port-binary/text-mode!
 
             ;; Definitions from fports.c.
             open-file
