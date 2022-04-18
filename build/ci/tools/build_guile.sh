8bab9d2326a2199317269f4425814db40e3eeba0
Larry Valkama <larryvalkama@protonmail.com>
2022-04-19 01:04:09 +0200

    github CI 11 run configure

---
+++ build/ci/tools/build_guile.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+echo setup mingw64 environment
+
+C=`pwd`
+export MSYSTEM=mingw64
+export PATH=$C/msys64/mingw64/bin:$C/msys64/mingw64/lib:$C/msys64/usr/bin
+
+export CFLAGS=-I$C/msys64/mingw64/x86_64-w64-mingw32/include
+export LIBS=-L$C/msys64/mingw64/x86_64-w64-mingw32/lib
+export PKG_CONFIG_PATH=$C/msys64/mingw64/lib/pkgconfig
+
+./configure --prefix=$C/mingw64 --host=x86_64-w64-mingw32 \
+            --disable-silent-rules --enable-jit=no \
+            --enable-guile-debug --enable-mini-gmp --with-bdw-gc=bdw-gc \
+            host_alias=x86_64-w64-mingw32 \
+            --no-create --no-recursion
