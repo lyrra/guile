3b7fea24189a754907f041d2ab98c51f01f25842
Larry Valkama <larryvalkama@protonmail.com>
2022-04-18 23:47:52 +0200

    github CI 5

--- build/ci/tools/package_mingw.sh
+++ build/ci/tools/package_mingw.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-pacman -S base-devel mingw-w64-x86_64-toolchain \
+pacman -v --noconfirm -S base-devel mingw-w64-x86_64-toolchain \
 mingw-w64-x86_64-xpm-nox mingw-w64-x86_64-libtiff \
 mingw-w64-x86_64-giflib mingw-w64-x86_64-libpng \
 mingw-w64-x86_64-libjpeg-turbo mingw-w64-x86_64-librsvg \
