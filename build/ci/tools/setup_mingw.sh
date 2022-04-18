31c06aaa4d509e9aeeeae0b674ddf0f1f40ee857
Larry Valkama <larryvalkama@protonmail.com>
2022-04-18 22:50:14 +0200

    github CI

---
+++ build/ci/tools/setup_mingw.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.tar.xz || exit 1
+tar xvf msys2-base-x86_64-latest.tar.xz || exit 1
+
