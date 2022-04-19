2cef043ab5ebac87df750c4bda272ef86bf8279d
Larry Valkama <larryvalkama@protonmail.com>
2022-04-19 11:45:57 +0200

    ci

--- build/ci/tools/build_guile.sh
+++ build/ci/tools/build_guile.sh
@@ -21,6 +21,8 @@ echo Running configure
 ./configure --prefix=$C/mingw64 --host=x86_64-w64-mingw32 \
             --disable-silent-rules --enable-jit=no \
             --enable-guile-debug --enable-mini-gmp --with-bdw-gc=bdw-gc \
-            host_alias=x86_64-w64-mingw32 \
-            --no-create --no-recursion || exit 1
+            host_alias=x86_64-w64-mingw32 || exit 1
+ls -ltr
+echo Building
+
 make || exit 1
