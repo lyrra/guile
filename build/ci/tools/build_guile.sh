aed2b04329b7bcd11be9d92be566d3877d32a490
Larry Valkama <larryvalkama@protonmail.com>
2022-04-19 06:42:20 +0200

    github CI 12 autogen

--- build/ci/tools/build_guile.sh
+++ build/ci/tools/build_guile.sh
@@ -10,6 +10,12 @@ export CFLAGS=-I$C/msys64/mingw64/x86_64-w64-mingw32/include
 export LIBS=-L$C/msys64/mingw64/x86_64-w64-mingw32/lib
 export PKG_CONFIG_PATH=$C/msys64/mingw64/lib/pkgconfig
 
+ls -ltr
+echo Running autogen.sh
+sh autogen.sh || exit 1
+ls -ltr
+echo Running configure
+
 ./configure --prefix=$C/mingw64 --host=x86_64-w64-mingw32 \
             --disable-silent-rules --enable-jit=no \
             --enable-guile-debug --enable-mini-gmp --with-bdw-gc=bdw-gc \
