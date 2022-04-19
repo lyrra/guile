3ce67c6ae889773d4eb8e9fcc4dcb297a58954e3
Larry Valkama <larryvalkama@protonmail.com>
2022-04-19 20:22:46 +0200

    ci build

--- build/ci/tools/build_guile.sh
+++ build/ci/tools/build_guile.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-git branch
-
 echo setup mingw64 environment
 
 C=`pwd`
@@ -12,17 +10,8 @@ export CFLAGS=-I$C/msys64/mingw64/x86_64-w64-mingw32/include
 export LIBS=-L$C/msys64/mingw64/x86_64-w64-mingw32/lib
 export PKG_CONFIG_PATH=$C/msys64/mingw64/lib/pkgconfig
 
-ls -ltr
-echo Running autogen.sh
-sh autogen.sh || exit 1
-ls -ltr
-echo Running configure
-
-./configure --prefix=$C/mingw64 --host=x86_64-w64-mingw32 \
-            --disable-silent-rules --enable-jit=no \
-            --enable-guile-debug --enable-mini-gmp --with-bdw-gc=bdw-gc \
-            host_alias=x86_64-w64-mingw32 || exit 1
-ls -ltr
 echo Building
 
-make || exit 1
+make
+
+cat libguile/vm-operations.h
