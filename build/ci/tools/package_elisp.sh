812c30ff5b18d2740576d0273edf4ff769ec10c6
Larry Valkama <lz64@proton.me>
2023-12-31 21:30:20 +0100

    ci package guile-elisp

---
+++ build/ci/tools/package_elisp.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+ls -ltr
+echo "------- build -------"
+ls -ltr build
+
+echo "------ Files to zip: ---------"
+
+echo "------ zipping files: ---------"
+
+find . | \
+     grep -e '^./bootstrap/.*\.go$' \
+          -e '^./doc/' \
+          -e '^./emacs/' \
+          -e '^./examples/' \
+          -e '^./guix/' \
+          -e '^./meta/' \
+          -e '^./guile-readline/.libs/guile-readline.a$' \
+          -e '^./guile-readline/.*\.go$' \
+          -e '^./lib/.libs/libgnu.a$' \
+          -e '^./libguile/.libs/guile.exe$' \
+          -e '^./libguile/.libs/gen-scmconfig.exe$' \
+          -e '^./libguile/.libs/guile_filter_doc_snarfage.exe$' \
+          -e '^./libguile/.libs/libguile-3.0-1.dll$' \
+          -e '^./libguile/.libs/libguile-3.0.a$' \
+          -e '^./libguile/.libs/libguile-3.0.dll.a$' \
+          -e '^./libguile/.*\.h$' \
+          -e '^./libguile.h$' \
+          -e '^./module/.*\.go$' \
+          -e '^./*.tar.zst$' \
+          -e '^./README$' \
+          -e '^./COPYING$' \
+          -e '^./INSTALL$' \
+          -e '^./THANKS$' \
+     | tar cvf linux-x86_64-guile3-elisp.tar.xz --files-from=-
+
+ls -ltr linux-x86_64-guile3-elisp.tar.xz
