lyrra/guile

commit b8031fc9653a039c4ba8c07b93e649155351657a

Exclude the finalizer thread from the ‘all-threads’ result.
Fixes <https://bugs.gnu.org/76343>.

Fixes a bug whereby “echo '(environ)' | guile” would wrongfully trigger
the multiple-thread warning.

* libguile/finalizers.c (finalizer_thread): New variable.
(finalization_thread_proc): Set it.
(scm_i_is_finalizer_thread): New function.
(run_finalization_thread): Clear FINALIZER_THREAD.
* libguile/finalizers.h (scm_i_is_finalizer_thread): New declaration.
* libguile/threads.c (scm_all_threads): Use it.
* NEWS: Update.

Reported-by: Simon Josefsson <simon@josefsson.org>

libguile/finalizers.c
@@ -1,4 +1,4 @@
-/* Copyright 2012-2014,2018-2020,2022
+/* Copyright 2012-2014,2018-2020,2022,2025
      Free Software Foundation, Inc.
 
    This file is part of Guile.
@@ -37,6 +37,7 @@
 #include "gsubr.h"
 #include "init.h"
 #include "threads.h"
+#include "atomics-internal.h"
 
 #include "finalizers.h"
 
@@ -217,10 +218,14 @@ read_finalization_pipe_data (void *data)
 
   return NULL;
 }
-  
+
+static scm_i_pthread_t finalizer_thread;
+
 static void*
 finalization_thread_proc (void *unused)
 {
+  scm_atomic_set_pointer ((void **) &finalizer_thread,
+                          (void *) pthread_self ());
   while (1)
     {
       struct finalization_pipe_data data;
@@ -255,10 +260,20 @@ finalization_thread_proc (void *unused)
     }
 }
 
+int
+scm_i_is_finalizer_thread (struct scm_thread *t)
+{
+  scm_i_pthread_t us =
+    (scm_i_pthread_t) scm_atomic_ref_pointer ((void **) &finalizer_thread);
+  return pthread_equal (t->pthread, us);
+}
+
 static void*
 run_finalization_thread (void *arg)
 {
-  return scm_with_guile (finalization_thread_proc, arg);
+  void *res = scm_with_guile (finalization_thread_proc, arg);
+  scm_atomic_set_pointer ((void **) &finalizer_thread, NULL);
+  return res;
 }
 
 static void

libguile/finalizers.h
@@ -1,7 +1,7 @@
 #ifndef SCM_FINALIZERS_H
 #define SCM_FINALIZERS_H
 
-/* Copyright 2012, 2013, 2014, 2018
+/* Copyright 2012, 2013, 2014, 2018, 2025
      Free Software Foundation, Inc.
 
    This file is part of Guile.
@@ -41,6 +41,9 @@ SCM_INTERNAL void scm_i_finalizer_pre_fork (void);
    called from a finalizer, which may be from an async or from another
    thread. */
 SCM_INTERNAL void scm_i_register_async_gc_callback (void (*callback) (void));
+
+/* Return true if THREAD is the finalizer thread.  */
+SCM_INTERNAL int scm_i_is_finalizer_thread (struct scm_thread *thread);
 
 SCM_API int scm_set_automatic_finalization_enabled (int enabled_p);
 SCM_API int scm_run_finalizers (void);

libguile/threads.c
@@ -1,4 +1,4 @@
-/* Copyright 1995-1998,2000-2014,2018-2019,2023-2024
+/* Copyright 1995-1998,2000-2014,2018-2019,2023-2025
      Free Software Foundation, Inc.
 
    This file is part of Guile.
@@ -47,6 +47,7 @@
 #include "dynwind.h"
 #include "eval.h"
 #include "extensions.h"
+#include "finalizers.h"
 #include "fluids.h"
 #include "gc-inline.h"
 #include "gc.h"
@@ -1691,7 +1692,9 @@ SCM_DEFINE (scm_all_threads, "all-threads", 0, 0, 0,
 
   for (t = all_threads; t && n > 0; t = t->next_thread)
     {
-      if (!t->exited && !scm_i_is_signal_delivery_thread (t))
+      if (!t->exited
+          && !scm_i_is_signal_delivery_thread (t)
+          && !scm_i_is_finalizer_thread (t))
 	{
 	  SCM_SETCAR (*l, t->handle);
 	  l = SCM_CDRLOC (*l);

commit 462d3c85edece865157f9771443ad929bc89d858

Provide ‘scm_i_is_finalizer_thread’ when building ‘--without-threads’.
Fixes a regression in ‘--without-threads’ builds introduced in
b8031fc9653a039c4ba8c07b93e649155351657a.

* libguile/finalizers.c (scm_i_is_finalizer_thread) [!SCM_USE_PTHREAD_THREADS]:
New function.

libguile/finalizers.c
@@ -327,7 +327,15 @@ spawn_finalizer_thread (void)
   start_finalization_thread ();
 }
 
-#endif /* SCM_USE_PTHREAD_THREADS */
+#else /* !SCM_USE_PTHREAD_THREADS */
+
+int
+scm_i_is_finalizer_thread (struct scm_thread *t)
+{
+  return 0;
+}
+
+#endif /* !SCM_USE_PTHREAD_THREADS */
 
 
 