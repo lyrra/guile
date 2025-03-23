d8f2eff89f37779d0960ae3c846849f394e1534f
Larry Valkama <lz64@proton.me>
2025-03-23 21:48:17 +0100

    add mingw-w64-x86_64-gcc-libs

--- build/ci/tools/setup_mingw.sh
+++ build/ci/tools/setup_mingw.sh
@@ -5,3 +5,5 @@ choco install -y wget
 
 wget https://github.com/msys2/msys2-installer/releases/download/nightly-x86_64/msys2-base-x86_64-latest.tar.xz || exit 1
 tar xvf msys2-base-x86_64-latest.tar.xz || exit 1
+
+pacman -S mingw-w64-x86_64-gcc-libs
